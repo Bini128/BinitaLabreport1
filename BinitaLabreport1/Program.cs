8124b2da1b034ded84839f90b34bda2bfa1bf38d
Bini128 <lamichhanenita98@gmail.com>
2024-08-13 18:16:48 +0545

    Add project files.

---
+++ BinitaLabreport1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinitaLabreport1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your fullname: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Welcome {userName}, to dotnet technology.");
+            Console.ReadKey();
+            Console.Read();
+        }
+    }
+}
