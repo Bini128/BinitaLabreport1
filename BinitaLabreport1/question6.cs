7c5867b0c358a42562770a869ef682409661f71a
Bini128 <lamichhanenita98@gmail.com>
2024-08-13 19:56:59 +0545

    another

---
+++ BinitaLabreport1/question6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinitaLabreport1
+{
+    internal class question6
+    {
+        static void Main()
+        {
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Before swapping: num1 = {num1}, num2 = {num2}");
+            double temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine($"After swapping: num1 = {num1}, num2 = {num2}");
+            Console.ReadKey();
+            Console.Read();
+        }
+    }
+}
+
+
